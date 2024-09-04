b96764f222b7752d68fc1df904397151de1fab9c
hoangnt2004 <137685481+hoangnt2004@users.noreply.github.com>
2024-09-04 21:00:56 +0700

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--){
+		int num , originalNum , result = 0, n = 0;
+		cin >> num;
+		originalNum = num ;
+		while(originalNum > 0){
+			originalNum /= 10;
+			n++;
+		}
+		originalNum = num;
+		while(originalNum > 0){
+			result += pow(originalNum % 10, n);
+			originalNum /= 10;
+		}
+		if(result == num){
+			cout <<"1"<<endl;
+		}else{
+			cout <<"0"<<endl;
+		}
+	}
+	return 0;
+}

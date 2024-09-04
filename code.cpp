fddbd14d982c00acc135df33a59672e9b0180682
hoangnt2004 <137685481+hoangnt2004@users.noreply.github.com>
2024-09-04 20:29:04 +0700

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+    char maNhanVien [5];
+	char hoten[50];
+	int luongCoBan ;
+	int soNgayCong;
+	char chucvu[5];
+	int tienthuong;
+	int phucap;
+	int thunhap;
+}employee;

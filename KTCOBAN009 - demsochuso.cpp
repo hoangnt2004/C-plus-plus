ceddf828f38874583bf4cfc971f0c74f9238c757
hoangnt2004 <137685481+hoangnt2004@users.noreply.github.com>
2024-09-20 23:21:50 +0700

    Add files via upload

---
+++ KTCOBAN009 - demsochuso.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+		string s;
+		cin >> s;
+		int dem =0;
+		for(int i = 0; i < s.size(); i++){
+			if(isdigit(s[i])){
+				dem++;
+			}
+		}
+		cout <<dem<<endl;
+	}
+return 0;
+}

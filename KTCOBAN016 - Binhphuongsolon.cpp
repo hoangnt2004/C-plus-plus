b1dc68e8ca8585cbc153ebe14a977dd310b1198b
hoangnt2004 <137685481+hoangnt2004@users.noreply.github.com>
2024-09-20 23:25:27 +0700

    Add files via upload

---
+++ KTCOBAN016 - Binhphuongsolon.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+		int n;
+		cin >> n;
+		for(int i = 1; i <= (n-1)/9 ; i++){
+			cout<<"123456790";
+		}
+		int k = (n-1)%9;
+		for(int i = 1; i <= k ;i++){
+			cout << char('0'+i);
+		}
+		for(int i = k + 1; i >= 2; i--){
+			cout << char('0'+i);
+		}
+		for(int i = 1; i <= (n-1)/9; i++){
+			cout<<"098765432";
+		}
+		cout<<1<<"\n";
+	}
+	return 0;
+}
